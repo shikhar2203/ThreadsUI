36b63d949725a04ad524c4228d37f542f20a76d4
shikhar2203 <shikharsharma2203@gmail.com>
2023-09-03 16:57:36 +0530

    Initial Commit

---
+++ ThreadsUI/ThreadsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsUIApp.swift
+//  ThreadsUI
+//
+//  Created by shikhar on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
